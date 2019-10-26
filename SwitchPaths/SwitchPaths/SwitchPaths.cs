eed84927b8a4dac859ad6e066f5e5368f210c1b4
Kyle Jones <45873014+KishiKyle@users.noreply.github.com>
2019-10-26 14:54:00 +0200

    Add files via upload

---
+++ SwitchPaths/SwitchPaths/SwitchPaths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwitchPaths
+{
+    public class SwitchPaths
+    {
+
+
+        public List<string[]> GetAllPaths(string[] file)
+        {
+            List<string[]> returnList = new List<string[]> ();
+
+            string SwitchSyntax = "SWITCH", EndSyntax = "ENDSWITCH", CaseSyntax = "CASE";
+
+
+
+
+            return returnList;
+        }
+    }
+
+    public class PathNode
+    {
+
+    }
+
+}
